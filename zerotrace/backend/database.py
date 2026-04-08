d4be85002d06ac7e9d2b83a24267a6b32c429d95
Raviraahul <raviraahul.prof@gmail.com>
2026-04-08 23:33:16 +0530

    rr commits backend

--- zerotrace/backend/database.py
+++ zerotrace/backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://USER:PASSWORD@HOST:PORT/postgres"
+DATABASE_URL = "postgresql://postgres:password@db.jrxasaqmeptvynaddcve.supabase.co:5432/postgres"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
